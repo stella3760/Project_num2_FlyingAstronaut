670c6d4396bd581000eea462c65a37099dda7976
Seunghye.Stella.Kim <120572485+stella3760@users.noreply.github.com>
2023-02-16 18:19:38 +0000

    ver 1.1

---
+++ MakePipe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(7, Random.Range(-2.1f, 5.0f), 0);
+            timer = 0;
+            Destroy(newpipe, 10.0f);
+        }
+    }
+}
